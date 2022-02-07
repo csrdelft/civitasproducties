40a850807d3a11436b98b246356a92f851c0bd0a
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2022-02-07 22:14:46 +0100

    Maak bestelformulier

--- src/assets/ticketverkoop.js
+++ src/assets/ticketverkoop.js
@@ -1,3 +1,38 @@
 import './ticketverkoop.scss'
 
 import 'bootstrap'
+
+
+let form = document.querySelector('#bestel-form');
+let errorContainer = form.querySelector('.errors');
+form.addEventListener('input', () => errorContainer.classList.add('d-none'));
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    form.submitButton.disabled = true;
+
+    console.log(form.action)
+    fetch(form.action, {
+        method: "POST",
+        body: new FormData(form)
+    })
+        .then(response => {
+            if (response.ok) {
+                form.reset();
+                errorContainer.classList.remove('d-none', 'alert-danger');
+                errorContainer.classList.add('alert-success');
+            } else {
+                errorContainer.classList.remove('d-none', 'alert-success');
+                errorContainer.classList.add('alert-danger');
+            }
+            return response
+        })
+        .then(response => response.text())
+        .then(body => {
+            errorContainer.textContent = body
+        })
+        .then(() => {
+            form.submitButton.disabled = false;
+        })
+});
